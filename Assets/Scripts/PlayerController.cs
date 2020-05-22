13187c76053724f86dc72159224c84545dd1b26e
Stéphane HULOT <stephane.hulot@telecomnancy.eu>
2020-05-22 16:32:16 +0200

    Le joueur peut attraper les vaches

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody body;
     public float speed = 20;
     public float friction = 10;
+    private GameObject grabbedCow;
 
     private void Start()
     {
@@ -15,6 +16,34 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        print(grabbedCow);
+        //attrape vache
+        if(Input.GetButtonDown("Jump"))
+        {
+            //trouve la vache la plus proche
+            GameObject[] cows = GameObject.FindGameObjectsWithTag("Cow");
+            if(cows.Length != 0)
+            {
+                float nearestDist = 4;
+                for (int i = 0; i < cows.Length; i++)
+                {
+                    float dist = Vector3.SqrMagnitude(transform.position - cows[i].transform.position);
+                    if (dist < nearestDist)
+                    {
+                        grabbedCow = cows[i];
+                        dist = nearestDist;
+                    }
+                }
+            }
+        }
+
+        //relache vache
+        if (Input.GetButtonUp("Jump"))
+        {
+            grabbedCow = null;
+        }
+
+        //animation du joueur
         transform.eulerAngles = new Vector3(-body.velocity.z, 0, body.velocity.x) * 4;
     }
 
@@ -25,6 +54,12 @@ public class PlayerController : MonoBehaviour
             dir.Normalize();
         body.AddForce(dir * speed);
         body.AddForce(FlattenVector(-body.velocity * friction));
+
+        if(grabbedCow != null)
+        {
+            Vector3 cowDir = (transform.position - grabbedCow.transform.position);
+            grabbedCow.GetComponent<Rigidbody>().AddForce(cowDir * cowDir.magnitude * 10);
+        }
     }
 
     public Vector3 FlattenVector(Vector3 vec)
