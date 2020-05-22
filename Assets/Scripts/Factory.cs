445c1dc9511cdb4970bf74dfa0252091a277ae77
Timothee WRIGHT <timothee.wright@telecomnancy.eu>
2020-05-22 21:36:28 +0200

    script factory

--- Assets/Scripts/Factory.cs
+++ Assets/Scripts/Factory.cs
@@ -6,7 +6,7 @@ public class Factory : MonoBehaviour
 {
 	public GameObject cowPrefab; 
 	//Production rate in Cows/seconds
-	private float productionRate = 0.2f;
+	private float productionRate = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +22,7 @@ public class Factory : MonoBehaviour
 	IEnumerator produceCows() {
 		while (true) {
 			yield return new WaitForSeconds(1/productionRate);
-			productionRate += 0.02f;
+			productionRate += 0.01f;
 			GameObject cow = Instantiate(cowPrefab, transform.position, Quaternion.Euler(0, 90, 0));
 			cow.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5f);
 		}
