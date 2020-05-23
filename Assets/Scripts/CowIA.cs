a4fa791872d19bf65d0e08ada42c8f367adc2085
Timothee WRIGHT <timothee.wright@telecomnancy.eu>
2020-05-23 11:27:32 +0200

    cows are attracted by hayStraws

--- Assets/Scripts/CowIA.cs
+++ Assets/Scripts/CowIA.cs
@@ -26,10 +26,42 @@ public class CowIA : MonoBehaviour
 		while (true)
 		{
 			yield return new WaitForSeconds(Random.Range(20, 60) / 15f);
-			rb.AddForce(new Vector3(Random.Range(-100, 100) / 100f, 2, Random.Range(-100, 100) / 100f) * 100);
+
+			Transform closestHayStraw = getClosestHayStraw();
+
+			if(closestHayStraw == null)
+				rb.AddForce(new Vector3(Random.Range(-100, 100) / 100f, 2, Random.Range(-100, 100) / 100f) * 100);
+			else
+			{
+				//rb.AddForce(new Vector3(closestHayStraw.position.x + Random.Range(-25, 25) / 100f, 2, closestHayStraw.position.z + Random.Range(-25, 25) / 100f) * 100);
+				Vector3 dir = new Vector3(closestHayStraw.position.x - rb.position.x, 0, closestHayStraw.position.z - rb.position.z);
+
+				rb.AddForce(new Vector3(dir.x + Random.Range(-50, 50) / 50f, 20, dir.z + Random.Range(-50, 50) / 50f) *10);
+			}
+				
 			GetComponent<AudioSource>().pitch = Random.Range(80, 120) / 100f;
 			GetComponent<AudioSource>().Play();
 		}
+	}
+
+	public Transform getClosestHayStraw()
+	{
+		Transform closestHayStraw = null;
+		GameObject[] hayStraws = GameObject.FindGameObjectsWithTag("straw");
+		if (hayStraws.Length != 0)
+		{
+			float nearestDist = 10;
+			for (int i = 0; i < hayStraws.Length; i++)
+			{
+				float dist = Vector3.SqrMagnitude(transform.position - hayStraws[i].transform.position);
+				if (dist < nearestDist)
+				{
+					closestHayStraw = hayStraws[i].GetComponent<Transform>();
+					dist = nearestDist;
+				}
+			}
+		}
+		return closestHayStraw;
 	}
 
 }
