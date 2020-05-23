8f4de94fc4c860c95e4c2fbf10a6783817b15eae
Stéphane HULOT <stephane.hulot@telecomnancy.eu>
2020-05-23 12:18:53 +0200

    Fix ufo

--- Assets/Scripts/UfoControl.cs
+++ Assets/Scripts/UfoControl.cs
@@ -17,25 +17,30 @@ public class UfoControl : MonoBehaviour
         while (true)
         {
             GameObject[] cows = GameObject.FindGameObjectsWithTag("Cow");
-            int i = Random.Range(0, cows.Length - 1);
+            while (cows.Length == 0)
+            {
+                yield return new WaitForSeconds(1);
+                cows = GameObject.FindGameObjectsWithTag("Cow");
+            }
+            int i = Random.Range(0, cows.Length);
             GameObject cow = cows[i];
             Vector3 endPos = new Vector3(cow.transform.position.x, transform.position.y, cow.transform.position.z);
 
             float t = 0;
             while (t < 1)
             {
-                t += Time.deltaTime * 2;
+                endPos = new Vector3(cow.transform.position.x, transform.position.y, cow.transform.position.z);
+                t += Time.deltaTime;
                 transform.position = Vector3.Lerp(transform.position, endPos, t);
                 yield return new WaitForEndOfFrame();
             }
 
-            yield return new WaitForSeconds(1);
             t = 0;
             while (t < 1)
             {
-                t += Time.deltaTime * 2;
+                t += Time.deltaTime;
                 cow.GetComponent<Rigidbody>().isKinematic = true;
-                cow.transform.position = transform.position + Vector3.down *((1-t)*transform.position.y);
+                cow.transform.position = transform.position + Vector3.down * (((1-t)*(transform.position.y-1.5f))+1);
                 yield return new WaitForEndOfFrame();
             }
 
@@ -45,14 +50,13 @@ public class UfoControl : MonoBehaviour
             t = 0;
             while (t < 1)
             {
-                
-                t += Time.deltaTime * 2;
+                t += Time.deltaTime;
                 transform.position = Vector3.Lerp(transform.position, endPos, t);
-                cow.transform.position = transform.position + Vector3.down*t;
+                cow.transform.position = transform.position + Vector3.down;
                 yield return new WaitForEndOfFrame();
             }
             cow.GetComponent<Rigidbody>().isKinematic = false;
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(5);
         }
         
     }
