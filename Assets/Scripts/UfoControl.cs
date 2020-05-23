9a21b93891a0c78a93afce5a1621c35e21fc440e
Merlin <merlin.baudou@telecomnancy.eu>
2020-05-23 12:48:11 +0200

    ajout bridgeleS

--- Assets/Scripts/UfoControl.cs
+++ Assets/Scripts/UfoControl.cs
@@ -27,11 +27,11 @@ public class UfoControl : MonoBehaviour
             Vector3 endPos = new Vector3(cow.transform.position.x, transform.position.y, cow.transform.position.z);
 
             float t = 0;
-            while (t < 1)
+            while (t < 2)
             {
                 endPos = new Vector3(cow.transform.position.x, transform.position.y, cow.transform.position.z);
                 t += Time.deltaTime;
-                transform.position = Vector3.Lerp(transform.position, endPos, t);
+                transform.position = Vector3.Lerp(transform.position, endPos, t * 0.1f);
                 yield return new WaitForEndOfFrame();
             }
 
@@ -48,10 +48,10 @@ public class UfoControl : MonoBehaviour
             GameObject hooker = GameObject.Find("Hooker");
             endPos = new Vector3(hooker.transform.position.x, transform.position.y, hooker.transform.position.z);
             t = 0;
-            while (t < 1)
+            while (t < 2)
             {
                 t += Time.deltaTime;
-                transform.position = Vector3.Lerp(transform.position, endPos, t);
+                transform.position = Vector3.Lerp(transform.position, endPos, t*0.1f);
                 cow.transform.position = transform.position + Vector3.down;
                 yield return new WaitForEndOfFrame();
             }
