d8e90fae0410c14b6843409348f18d42c41e989b
Stéphane HULOT <stephane.hulot@telecomnancy.eu>
2020-05-23 11:07:26 +0200

    Amelioration de la construction

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -5,32 +5,76 @@ using UnityEngine;
 public class BuildingManager : MonoBehaviour
 {
     public GameObject fence;
-    public Transform ghostFence;
+    public Transform ghostItems;
     private bool rotate = false;
+    private int currentItem;
+
+    public Material canBuild;
+    public Material cannotBuild;
+
+    private void Start()
+    {
+        for (int i = 0; i < ghostItems.childCount; i++)
+        {
+            ghostItems.GetChild(i).gameObject.SetActive(i == 0);
+        }
+    }
 
     private void Update()
     {
         if (!GameManager.gameStarted)
             return;
 
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                currentItem++;
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                currentItem--;
+            if (currentItem < 0)
+                currentItem = ghostItems.childCount - 1;
+            if (currentItem >= ghostItems.childCount)
+                currentItem = 0;
+
+            for(int i = 0; i < ghostItems.childCount; i++)
+            {
+                ghostItems.GetChild(i).gameObject.SetActive(i == currentItem);
+            }
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
             rotate = !rotate;
-            ghostFence.eulerAngles = new Vector3(0, rotate ? 90 : 0, 0);
+            ghostItems.eulerAngles = new Vector3(0, rotate ? 90 : 0, 0);
         }
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit) && hit.transform.CompareTag("Buildable"))
         {
-            ghostFence.position = hit.point;
-            if (Input.GetMouseButtonDown(0) && GameManager.instance.steakCount >= 3)
+            ghostItems.position = hit.point;
+            if(GameManager.instance.steakCount >= 3)
             {
-                GameManager.instance.RemoveSteaks(3);
-                Instantiate(fence, hit.point, ghostFence.rotation);
+                for (int i = 0; i < ghostItems.childCount; i++)
+                {
+                    ghostItems.GetChild(i).GetComponent<MeshRenderer>().material = canBuild;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GameManager.instance.RemoveSteaks(3);
+                    Instantiate(fence, hit.point, ghostItems.rotation);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < ghostItems.childCount; i++)
+                {
+                    ghostItems.GetChild(i).GetComponent<MeshRenderer>().material = cannotBuild;
+                }
             }
         }
         else
-            ghostFence.position = Vector3.down * 2;
+            ghostItems.position = Vector3.down * 2;
     }
 }
