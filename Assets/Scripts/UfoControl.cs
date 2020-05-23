aebdc17357059fd02c59600f365c9a21dc8b3911
Merlin <merlin.baudou@telecomnancy.eu>
2020-05-23 11:01:11 +0200

    script ufo

---
+++ Assets/Scripts/UfoControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(TranscendCow());
+    }
+
+    IEnumerator TranscendCow()
+    {
+        GameObject[] cows = GameObject.FindGameObjectsWithTag("Cow");
+        int i = Random.Range(0, cows.Length - 1);
+        GameObject cow = cows[i];
+        Vector3 endPos = new Vector3(cow.transform.position.x, transform.position.z, cow.transform.position.y);
+
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 2;
+            transform.position = Vector3.Lerp(transform.position, endPos, t);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
